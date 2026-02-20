4bec4c96f6f264af3fa5f991983673e6eadcf354
khoroleets <vladkhorolets4847@gmail.com>
2026-02-20 19:08:57 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./*.html"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#27AE60',
+        secondary: '#2F80ED',
+        // Світла тема
+        neutralBg: '#F5F5F5',
+        neutralWhite: '#FFFFFF',
+        neutralText: '#333333',
+        // Темна тема
+        darkBg: '#121212',
+        darkSurface: '#312E2E',
+        darkText: '#FFFFFF',
+        // Модальне вікно
+        modalRed: '#FF0000',
+		modalDarkRed: '#BC0404',
+      }
+    }
+  }
+}
